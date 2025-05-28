a63c2792cccfbdebc72d6b86099f97e02948d549
Adam Dehnel <arsdehnel@gmail.com>
2025-05-27 22:38:08 -0500

    fix: post cards use better flex-box setup

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function PostCard( { slug, title, date, excerpt } ) {
     return (
-        <div>
+        <div className="post-card">
             <h3><Link href={ slug }>{ title }</Link></h3>
             <div>Date: { date }</div>
             <p>{ excerpt }</p>
