b23e26b609daade97680ea88df9fc8e74ecf54ff
Adam Dehnel <arsdehnel@gmail.com>
2025-12-27 20:51:07 -0600

    fix: use an actual css grid for post listing page

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function PostCard( { slug, title, date, excerpt, coverImage, coverImageAltText } ) {
 
     return (
-        <Link href={ slug }>
-            <div className="post-card">
+        <Link href={ slug } className="post-card">
+            <div >
         
             <div className="post-content">
                 {
