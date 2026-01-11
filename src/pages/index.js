9fc59bfe4806262a8f292ca9897ae03e0fa4db70
Dorothy Toth <dorothytoth@me.com>
2026-01-11 17:20:28 -0600

    in prog: mixing up the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,31 @@ export default function Home({ latestPost }) {
 	return (
 		<>
 			<div className="home-columns">
-				<div>
+				<div className="intro">
 					<h1>👋 Welcome to my site!</h1>
 					<p>This site is about me in all aspects of life. It started as a place to share technology-related things I&apos;m curious about as they relate to my career but evolved into just something about me. It&apos;s important to know more about a person than just what code repositories they have and how they approach modern software development. This tries to cover what&apos;s important to me and how I approach all things in life, not just software and technology.</p>
-					<div className="site-section-cards">
+				</div>
+				<div className="site-section-cards">
 						<div className="site-section-card">
+							<img className='home-card-image' src="public/posts/camping-car-storage/initial-prototype.jpg" alt="alt text here" />
 							<h3>Father</h3>
 							<p>Being a father is the most important role I play in life.  The other roles exist to support this or really could be expressed as just a part of this.</p>
 						</div>
 						<div className="site-section-card">
+							<img className='home-card-image' src="public/posts/camping-car-storage/initial-prototype.jpg" alt="alt text here" />
 							<h3>Nerd</h3>
 							<p>Starting as a teenager in my parents&apos; basement breaking their 386 SX regularly I&apos;ve been a lifelong technology tinkerer.</p>
 						</div>
 						<div className="site-section-card">
+							<img className='home-card-image' src="public/posts/camping-car-storage/initial-prototype.jpg" alt="alt text here" />
 							<h3>Cook</h3>
 							<p>I certainly have childhood memories of food and helping in the kitchen but my passion for cooking has been on a slow simmer building throughout my life to where now it&apos;s something I think about all the time.</p>
 						</div>
 						<div className="site-section-card">
+							<img className='home-card-image' src="public/posts/camping-car-storage/initial-prototype.jpg" alt="alt text here" />
 							<h3>Woodworker</h3>
 							<p>My grandfather had a workshop in their basement and I have incredibly fond memories of spending time there.  I&apos;ve built up quite the shop myself now and love every minute I spend out there.</p>
 						</div>
-					</div>
 				</div>
 				<div>
 					<h2>Latest Post</h2>
