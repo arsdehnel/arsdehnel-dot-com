ca47ebcae2118e4254795a2afd012f7718d69c52
Dorothy Toth <dorothytoth@me.com>
2026-02-28 11:06:18 -0600

    broken font link?!

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
 			<Head>
 				<meta name="description" content="Personal site for Adam Dehnel" />
 				<link rel="icon" href="/favicon.ico" />
-				<link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet' />
+				<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel='stylesheet' />
 			</Head>
 			<body>
 				<Main />
