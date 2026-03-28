00ec416b5004ce689f77a51763248969dd98a94d
Adam Dehnel <arsdehnel@gmail.com>
2026-03-27 20:06:46 -0500

    feat: font-chooser

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,4 @@
+import classnames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { AiFillGithub, AiFillGitlab, AiFillLinkedin } from "react-icons/ai";
@@ -6,25 +7,60 @@ export default function Layout({ children, mainClass }) {
 	const router = useRouter();
 	return (
 		<div className="layout-wrapper">
+			<div
+				style={{
+					position: "sticky",
+					top: 0,
+					right: 0,
+					height: 20,
+					zIndex: 100,
+				}}
+			>
+				<button
+					type="button"
+					onClick={() => (document.body.className = "font-roboto")}
+				>
+					Roboto
+				</button>
+				<button
+					type="button"
+					onClick={() => (document.body.className = "font-inconsolata")}
+				>
+					Inconsolata
+				</button>
+				<button
+					type="button"
+					onClick={() => (document.body.className = "font-pt")}
+				>
+					PT Mono
+				</button>
+			</div>
 			<nav className="nav-main">
 				<Link
 					href="/posts"
-					className={router.pathname.startsWith("/posts") ? "active" : ""}
+					className={classnames({
+						active: router.pathname.startsWith("/posts"),
+						"secondary-link": true,
+					})}
 					id="secondary-link"
 				>
 					Posts
 				</Link>
 				<Link
 					href="/"
-					className={router.pathname === "/" ? "active" : ""}
-					id="home"
+					className={classnames({
+						active: router.pathname === "/",
+						"home-link": true,
+					})}
 				>
 					Adam Dehnel
 				</Link>
 				<Link
 					href="/about"
-					className={router.pathname === "/about" ? "active" : ""}
-					id="secondary-link"
+					className={classnames({
+						active: router.pathname === "/about",
+						"secondary-link": true,
+					})}
 				>
 					About
 				</Link>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,10 @@ export default function Document() {
 			<Head>
 				<meta name="description" content="Personal site for Adam Dehnel" />
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Stencil:opsz,wght@10..72,100..900&family=Inconsolata:wght@200..900&family=Oswald:wght@200..700&family=PT+Mono&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
